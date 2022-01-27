95a60336c060f059e9da202827d7f04a86be6b00
Shakilk Sarder <shakil01748@hotmail.com>
2022-01-28 00:23:34 +0600

    variable calculation

--- first.js
+++ first.js
@@ -1,1 +1,16 @@
-console.log(505);
+var apple = 80;
+var orange = 20;
+var rice = 65;
+
+var salePrice = 1000;
+var purchasePrice = 700;
+
+var profit = salePrice - purchasePrice;
+var appleSale = apple * 20;
+var totalSale = apple + orange + rice;
+
+
+console.log("Toatl Profit : ", profit, "Taka");
+console.log("Toatl Apple sold : ", appleSale, "Taka");
+console.log("Toatl Sale : ", totalSale, "Taka");
+
---
+++ variable.js
@@ -0,0 +1,8 @@
+var price = 500;
+var age = 25;
+var temp = 37;
+var name = "shakil sarder";
+var address = "nikunja-2 Khilkhet";
+var university = "AIUB";
+
+console.log(name);
