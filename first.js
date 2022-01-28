2c511813c5e4b1fe2851d8bf4563aa743cf4147b
Shakilk Sarder <shakil01748@hotmail.com>
2022-01-28 22:09:20 +0600

    practise

--- first.js
+++ first.js
@@ -10,7 +10,30 @@ var appleSale = apple * 20;
 var totalSale = apple + orange + rice;
 
 
-console.log("Toatl Profit : ", profit, "Taka");
-console.log("Toatl Apple sold : ", appleSale, "Taka");
-console.log("Toatl Sale : ", totalSale, "Taka");
+// console.log("Toatl Profit : ", profit, "Taka");
+// console.log("Toatl Apple sold : ", appleSale, "Taka");
+// console.log("Toatl Sale : ", totalSale, "Taka");
+
+// date function
+// var date = new Date();
+// console.log(date);
+
+
+// var one = 0.2;
+// var two = 0.3;
+// var sum = one + two;
+// console.log(sum);
+
+// Modulus (Vhag sesh)
+var amount = 100;
+var people = 3;
+// var equal = amount % people;
+var equal = amount / people;
+console.log(equal.toFixed(2));
+console.log(17 % 5);
+
+var num1 = "99.5";
+var num2 = 15;
+console.log(num1 + num2);
+
 
